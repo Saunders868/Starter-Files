7c836ec00c184fa1b765a75f2bb11e3a62301a30
Daniel_Saunders <saundersdaniel.10@gmail.com>
2021-08-20 13:34:36 -0400

    completed project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// css
+import "./scss/index.scss";
+
+// js
+import Person from "./js/Person";
+
+const person = new Person("John", 35);
+console.log(person.info());
+
+// any other npm installs can be added here as well
